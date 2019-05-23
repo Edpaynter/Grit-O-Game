d4fcccf2400485596497af66e917bb0d501116ef
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-05-23 19:51:18 -0400

    changed html routes to have test functionality

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,26 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Highscore.findAll({}).then(function (dbHighscores) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg: "Welcome!"
       });
     });
   });
 
-  app.get("/phaser", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/phaser", function (req, res) {
+    db.Highscore.findAll({}).then(function (dbHighscores) {
       res.render("phaser", {
-        msg: "Welcome!",
-        examples: dbExamples
+        msg: "Welcome!"
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -30,7 +28,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
